74d0c4e5f64d396aeb053bcaff7b3c654a41cb1e
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-09-06 22:08:05 +0600

    Implementation of prepare and reducer functions for data preparation.

--- src/redux/features/tasks/tasks.slice.ts
+++ src/redux/features/tasks/tasks.slice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { TInitialState } from "./tasks.type";
+import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
+import type { ITask, TInitialState } from "./tasks.type";
 
 const initialState: TInitialState = [];
 
@@ -7,9 +7,23 @@ const tasksSlice = createSlice({
     name: "tasks",
     initialState,
     reducers: {
-        addTask: (state, action) => {
-            state.push(action.payload);
-        },
+        addTask: {
+            prepare: (input:Pick<ITask, 'title' | 'description' | 'priority' | 'status'>) => {
+                const task = {
+                    id: nanoid(),
+                    title: input.title.trim(),
+                    description: input.description.trim(),
+                    priority: input.priority,
+                    status: input.status,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                };
+                return { payload: task };
+            },
+            reducer: (state, action : PayloadAction<ITask>) => {
+                state.push(action.payload);
+            }
+        }
     },
 });
 
--- src/redux/features/tasks/tasks.type.ts
+++ src/redux/features/tasks/tasks.type.ts
@@ -2,10 +2,13 @@ export type TTaskStatus = "pending" | "in-progress" | "done";
 export type TTaskPriority = "low" | "medium" | "high";
 
 export interface ITask {
+    id: string;
     title: string;
     description: string;
     priority: TTaskPriority;
     status: TTaskStatus;
+    createdAt: number;
+    updatedAt: number;
 }
 
 export type TInitialState = ITask[];
