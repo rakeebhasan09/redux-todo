b8d3e379768600f95ae6f86d07b13770ae7effac
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-09-09 22:43:30 +0600

    Write update and delete reducers code.

--- src/redux/features/tasks/tasks.slice.ts
+++ src/redux/features/tasks/tasks.slice.ts
@@ -15,14 +15,32 @@ const tasksSlice = createSlice({
                     description: input.description.trim(),
                     priority: input.priority,
                     status: input.status,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
+                    createdAt:  Date.now(),
+                    updatedAt:  Date.now(),
                 };
                 return { payload: task };
             },
             reducer: (state, action : PayloadAction<ITask>) => {
                 state.push(action.payload);
             }
+        },
+        updateTask: (state, action) => {
+            const {id, change} = action.payload;
+            const task = state.find((task) => task.id === id);
+            if (!task) return;
+            Object.assign(task, change, {updatedAt: Date.now()});
+        },
+        updateStatus: (state, action) => {
+            const {id, status} = action.payload;        
+            const task = state.find((task) => task.id === id);
+            if (!task) return;
+            task.status = status;
+            task.updatedAt = Date.now();
+        },
+        removeTask: (state, action) => {
+            const task = state.find((task) => task.id === action.payload);
+            if (!task) return;
+            return state.filter(item => item.id !== task.id);    
         }
     },
 });
