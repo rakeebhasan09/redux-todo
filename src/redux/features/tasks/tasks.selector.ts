d752a55a587991c8d43701c8c16293b4c10d7006
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-09-20 21:32:21 +0600

    Implementing task calculation using createSelector.

--- src/redux/features/tasks/tasks.selector.ts
+++ src/redux/features/tasks/tasks.selector.ts
@@ -1,7 +1,25 @@
 import type { RootState } from "@/redux/store";
+import { createSelector } from "@reduxjs/toolkit";
 
 export const selectAllTasks = (state: RootState) => state.tasks;
 export const selectTotalTasks = (state: RootState) => state.tasks.length;
 
 export const selectTaskById = (state: RootState, id: string) =>
     state.tasks.find((task) => task.id === id);
+
+export const selectTaskStats = createSelector([selectAllTasks], (tasks) => {
+    const stats = {
+        total: tasks.length,
+        byStatus: { pending: 0, "in-progress": 0, done: 0 },
+        byPriority: { high: 0, medium: 0, low: 0 },
+    };
+
+    for (const task of tasks) {
+        stats.byPriority[task.priority] += 1;
+        stats.byStatus[task.status] += 1;
+    }
+
+    stats.total = tasks.length;
+
+    return stats;
+});
