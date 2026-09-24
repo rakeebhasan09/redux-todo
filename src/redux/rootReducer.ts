b0aee0e2dfaee99854783347b08b801ac51856e5
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-09-24 23:28:20 +0600

    Initializing and try to  implement the redux-persist but got some errors. Now I try to fixup those errors.

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,8 +1,17 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasks.slice";
 import filtersReducer from "./features/filters/filters.slice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+    key: "redux-store",
+    storage,
+};
+
+export const combineReducer = combineReducers({
     tasks: tasksReducer,
     filters: filtersReducer,
 });
+
+export const rootReducer = persistReducer(persistConfig, combineReducer);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./rootReducer";
 import { logger } from "redux-logger";
+import { persistStore } from "redux-persist";
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
     devTools: import.meta.env.VITE_NODE_ENV === "development",
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
